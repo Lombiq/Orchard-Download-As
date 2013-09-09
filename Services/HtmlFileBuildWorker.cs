ef1d3d4b62139f510349b7e3bfc56139c9b9d536
Piedone <piedone@pyrocenter.hu>
2013-09-09 15:19:08 +0200

    Factoring out shape output capturing to service and adding wrapper shape around the downloaded shape

--- Services/HtmlFileBuildWorker.cs
+++ Services/HtmlFileBuildWorker.cs
@@ -12,16 +12,23 @@ using Lombiq.DownloadAs.Models;
 using Orchard;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Aspects;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.Implementation;
 using Orchard.Environment.Extensions;
 using Orchard.Localization;
 using Orchard.Mvc;
+using Orchard.Settings;
 
 namespace Lombiq.DownloadAs.Services
 {
     [OrchardFeature("Lombiq.DownloadAs.Html")]
     public class HtmlFileBuildWorker : IFileBuildWorker
     {
-        private readonly IWorkContextAccessor _wca;
+        private readonly ISiteService _siteService;
+        private readonly dynamic _shapeFactory;
+        private readonly IShapeOutputGenerator _shapeOutputGenerator;
+
+        private const string DisplayType = "File-html";
 
         private IFileBuildWorkerDescriptor _descriptor;
         public IFileBuildWorkerDescriptor Descriptor
@@ -43,9 +50,14 @@ namespace Lombiq.DownloadAs.Services
         public Localizer T { get; set; }
 
 
-        public HtmlFileBuildWorker(IWorkContextAccessor wca)
+        public HtmlFileBuildWorker(
+            ISiteService siteService,
+            IShapeFactory shapeFactory,
+            IShapeOutputGenerator shapeOutputGenerator)
         {
-            _wca = wca;
+            _siteService = siteService;
+            _shapeFactory = shapeFactory;
+            _shapeOutputGenerator = shapeOutputGenerator;
 
             T = NullLocalizer.Instance;
         }
@@ -54,124 +66,80 @@ namespace Lombiq.DownloadAs.Services
         public Stream Build(IEnumerable<IContent> contents)
         {
             var contentManager = contents.First().ContentItem.ContentManager;
-            var workContext = _wca.GetContext();
 
-            var controller = new DummyController(workContext.HttpContext.Request.RequestContext);
-
-            var stringBuilder = new StringBuilder();
-            using (var stringWriter = new StringWriter(stringBuilder))
-            using (var htmlTextWriter = new HtmlTextWriter(stringWriter))
-            {
-                var originalContext = controller.ControllerContext.HttpContext;
-
-                try
+            var aliases = contents
+                .Where(content => content.As<IAliasAspect>() != null)
+                .Select(content => new
                 {
-                    // Get the Request and User objects from the current, unchanged context
-                    var currentRequest = HttpContext.Current.ApplicationInstance.Context.Request;
-                    var currentUser = HttpContext.Current.ApplicationInstance.Context.User;
+                    Id = content.ContentItem.Id,
+                    Path = content.As<IAliasAspect>().Path
+                })
+                .ToDictionary(alias => alias.Path);
 
-                    // Create our new HttpResponse object containing our HtmlTextWriter
-                    var newResponse = new HttpResponse(htmlTextWriter);
+            var siteUri = new Uri(_siteService.GetSiteSettings().BaseUrl);
+            var contentShapes = contents.Select(content =>
+                {
+                    IShape contentShape = contentManager.BuildDisplay(content, DisplayType);
 
-                    // Create a new HttpContext object using our new Response object and the existing Request and User objects
-                    var newContext = new HttpContextWrapper(
-                                new HttpContext(currentRequest, newResponse)
-                                {
-                                    User = currentUser
-                                });
-
-                    // Swap in our new HttpContext object - output from this controller is now going to our HtmlTextWriter object
-                    controller.ControllerContext.HttpContext = newContext;
-
-                    var responseStream = new MemoryStream();
-                    var responseStreamWriter = new StreamWriter(responseStream);
-
-                    var aliases = contents
-                        .Where(content => content.As<IAliasAspect>() != null)
-                        .Select(content => new
-                            {
-                                Id = content.ContentItem.Id,
-                                Path = content.As<IAliasAspect>().Path
-                            })
-                        .ToDictionary(alias => alias.Path);
-
-                    foreach (var content in contents)
-                    {
-                        stringWriter.Write("<a name=\"content-item-top-" + content.ContentItem.Id + "\"></a>");
-
-                        new ShapePartialResult(controller, contentManager.BuildDisplay(content, "File-html"))
-                            .ExecuteResult(controller.ControllerContext);
-
-                        var html = stringBuilder.ToString();
-
-                        #region Adjust internal links
-                        var siteUri = new Uri(workContext.CurrentSite.BaseUrl);
-                        var urls = new HashSet<string>();
-                        var doc = new HtmlDocument();
-                        doc.LoadHtml(html);
-
-                        var links = doc.DocumentNode.SelectNodes("//a[@href]");
-                        if (links != null) // See: https://htmlagilitypack.codeplex.com/workitem/29175
+                    contentShape.Metadata.OnDisplayed(context =>
                         {
-                            var aliasAspect = content.As<IAliasAspect>();
-                            if (aliasAspect != null)
-                            {
-                                var itemUri = new Uri(siteUri, aliasAspect.Path);
-                                foreach (var link in links)
-                                {
-                                    var href = link.GetAttributeValue("href", null);
-                                    if (href != null)
-                                    {
-                                        Uri uri = null;
-                                        if (Uri.IsWellFormedUriString(href, UriKind.Relative))
-                                        {
-                                            uri = new Uri(itemUri, href);
-                                        }
-                                        else
-                                        {
-                                            var linkUri = new Uri(href);
-                                            if (linkUri.Host == siteUri.Host) uri = linkUri;
-                                        }
+                            var doc = new HtmlDocument();
+                            doc.LoadHtml(context.ShapeMetadata.ChildContent.ToHtmlString());
 
-                                        if (uri != null) // The uri is an internal one
+                            var links = doc.DocumentNode.SelectNodes("//a[@href]");
+                            if (links != null) // See: https://htmlagilitypack.codeplex.com/workitem/29175
+                            {
+                                var aliasAspect = content.As<IAliasAspect>();
+                                if (aliasAspect != null)
+                                {
+                                    var itemUri = new Uri(siteUri, aliasAspect.Path);
+                                    foreach (var link in links)
+                                    {
+                                        var href = link.GetAttributeValue("href", null);
+                                        if (href != null)
                                         {
-                                            var alias = uri.LocalPath.TrimStart('/');
-                                            if (aliases.ContainsKey(alias))
+                                            Uri uri = null;
+                                            if (Uri.IsWellFormedUriString(href, UriKind.Relative))
                                             {
-                                                link.SetAttributeValue("href", "#content-item-top-" + aliases[alias].Id);
+                                                uri = new Uri(itemUri, href);
                                             }
                                             else
                                             {
-                                                link.SetAttributeValue("href", uri.ToString());
+                                                var linkUri = new Uri(href);
+                                                if (linkUri.Host == siteUri.Host) uri = linkUri;
+                                            }
+
+                                            if (uri != null) // The uri is an internal one
+                                            {
+                                                var alias = uri.LocalPath.TrimStart('/');
+                                                if (aliases.ContainsKey(alias))
+                                                {
+                                                    link.SetAttributeValue("href", "#content-item-top-" + aliases[alias].Id);
+                                                }
+                                                else
+                                                {
+                                                    link.SetAttributeValue("href", uri.ToString());
+                                                }
                                             }
                                         }
                                     }
                                 }
                             }
-                        }
 
-                        doc.Save(responseStreamWriter);
-                        #endregion
+                            var stringBuilder = new StringBuilder();
+                            using (var stringWriter = new StringWriter(stringBuilder))
+                            {
+                                doc.Save(stringWriter);
+                            }
+                            context.ShapeMetadata.ChildContent = new HtmlString(stringBuilder.ToString());
+                        });
 
-                        stringBuilder.Clear();
-                    }
+                    return contentShape;
+                });
 
-                    responseStreamWriter.Flush();
-
-
-                    newResponse.Flush();
-                    htmlTextWriter.Flush();
-                    stringWriter.Flush();
-
-                    responseStream.Position = 0;
-                    return responseStream;
-                }
-                finally
-                {
-                    // Setting context back to original so nothing gets messed up
-                    controller.ControllerContext.HttpContext = originalContext;
-                }
-            }
+            var shape = _shapeFactory.DownloadAs_ContentsWrapper(ContentShapes: contentShapes);
+            shape.Metadata.Alternates.Add("DownloadAs_ContentsWrapper__html");
+            return _shapeOutputGenerator.GenerateOutput(shape);
         }
 
 
