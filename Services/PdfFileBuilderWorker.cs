3f251fda1e542c3a2163895e48d58ab6fdd215bf
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-09-12 00:15:51 +0200

    Experimental HTML to PDF generation with ITextSharp (PDF preserves only minimal formatting)
    --HG--
    branch : ITextSharp

--- Services/PdfFileBuilderWorker.cs
+++ Services/PdfFileBuilderWorker.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 using Lombiq.DownloadAs.Models;
 using Orchard.ContentManagement;
 using Orchard.Environment.Extensions;
@@ -10,7 +14,7 @@ using Orchard.Localization;
 
 namespace Lombiq.DownloadAs.Services
 {
-    [OrchardFeature("Lombiq.DownloadAs.Html")]
+    [OrchardFeature("Lombiq.DownloadAs.Pdf")]
     public class PdfFileBuilderWorker : IFileBuildWorker
     {
         private readonly IFlattenedHtmlGenerator _htmlGenerator;
@@ -36,7 +40,7 @@ namespace Lombiq.DownloadAs.Services
 
 
         public PdfFileBuilderWorker(IFlattenedHtmlGenerator htmlGenerator)
-        {  
+        {
             _htmlGenerator = htmlGenerator;
 
             T = NullLocalizer.Instance;
@@ -45,7 +49,33 @@ namespace Lombiq.DownloadAs.Services
 
         public Stream Build(IEnumerable<IContent> contents)
         {
-            return _htmlGenerator.GenerateHtml(contents, Descriptor.SupportedFileExtension);
+            string html;
+            using (var htmlStream = _htmlGenerator.GenerateHtml(contents, Descriptor.SupportedFileExtension))
+            using (var streamReader = new StreamReader(htmlStream))
+            {
+                html = streamReader.ReadToEnd();
+                html = html.Replace("<hr>", ""); // hr tags cause NE on worker.Parse()
+            }
+
+            using (var htmlReader = new StringReader(html))
+            using (var pdfStream = new MemoryStream())
+            using (var document = new Document(PageSize.A4, 80, 50, 30, 65))
+            using (var writer = PdfWriter.GetInstance(document, pdfStream))
+            using (var worker = new HTMLWorker(document))
+            {
+
+                document.Open();
+                worker.StartDocument();
+
+                worker.Parse(htmlReader);
+
+                worker.EndDocument();
+                worker.Close();
+                document.Close();
+
+                // This copying is needed because the document should be closed. This in turn however also disposes pdfStream.
+                return new MemoryStream(pdfStream.ToArray());
+            }
         }
     }
 }
